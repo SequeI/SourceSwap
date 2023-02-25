b0f1337cbbe188b0df1da3fc7d9e917aeda42fc7
virtus <AleksySi2509@protonmail.com>
2023-02-25 03:42:48 +0000

    Created db for registration

--- shop/__init__.py
+++ shop/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy 
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///gameshop.db"
 db = SQLAlchemy(app)
 app.config["SECRET_KEY"] = "2345235"
+bcrypt = Bcrypt(app)
 
 from shop.admin import route
