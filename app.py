dd139c5ae17f2a56e0f9901025478ccbfbd2b6f3
virtus <AleksySi2509@protonmail.com>
2023-02-28 02:12:55 +0000

    Changed secret key in app.config to be randomly set during session

--- app.py
+++ app.py
@@ -4,11 +4,12 @@ from flask_session import Session
 from werkzeug.security import generate_password_hash, check_password_hash
 from forms import RegistrationForm, LoginForm
 from functools import wraps
+import os
 
 
 app = Flask(__name__)
 app.teardown_appcontext(close_db)
-app.config["SECRET_KEY"] = "RANDOMBRUHHSDISAFD"
+app.config["SECRET_KEY"] = os.urandom(12)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
